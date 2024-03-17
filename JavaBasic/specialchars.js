a6f1326bb7c426f3f4e46e98de717b3a12315df9
Jaymeen Patel <pjaymeen908@gmail.com>
2024-03-17 13:10:02 +0530

    Special charater can be defines like a \n and \t

---
+++ JavaBasic/specialchars.js
@@ -0,0 +1,9 @@
+let text = "The new apple iphone \n has been launched";
+console.log(text);
+
+let text1 = "the new apple iphone 14 \t has been launched ";
+console.log(text1);
+
+let age = 20;
+let name = "Jaymeen";
+console.log(name, "\n", age);
