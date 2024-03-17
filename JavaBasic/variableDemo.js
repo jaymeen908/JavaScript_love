f63116ff50fa61611970a32982b9996d746a3922
Jaymeen Patel <pjaymeen908@gmail.com>
2024-03-17 11:38:57 +0530

    How to create a variables

---
+++ JavaBasic/variableDemo.js
@@ -0,0 +1,18 @@
+/**\
+ * 1 way to create a variables using `var` keywords
+ */
+
+var marks = 90;
+var score = 10;
+
+/**
+ * 2 way to create a variables using `let` keywords
+ */
+
+let age = 21;
+let flag = 0;
+
+/**
+ * 3 way to create a variables using `const` keywords
+ */
+const x = 97;
