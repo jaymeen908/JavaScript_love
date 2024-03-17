8429d70241bdc596f2f89b62d5801793eee9854c
Jaymeen Patel <pjaymeen908@gmail.com>
2024-03-17 13:05:57 +0530

    data types and how to print

---
+++ JavaBasic/datatypesDemo.js
@@ -0,0 +1,20 @@
+var marks = 100;
+var name = 'Jaymeen';
+var age;
+var company = null;
+var salary = undefined;
+var isStudent = true;
+console.log("Marks = ", marks);
+console.log("Name = ", name);
+console.log("age = ", age);
+console.log("Company = ", company);
+console.log("Salary = ",salary);
+console.log("Is A Student ? = ", isStudent);
+
+var user = {
+    name: "Jaymeen",
+    company: "Google",
+    age: 24
+}
+
+console.log("Details of thr user is ", user);
