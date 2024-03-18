dd12fb74873b25f6e0e5e9290c443c67e48f0a24
Jaymeen Patel <pjaymeen908@gmail.com>
2024-03-18 18:58:43 +0530

    Operators

---
+++ JavaBasic/operatorsdemo.js
@@ -0,0 +1,25 @@
+// arithmetic Operators
+let x = 10;
+let y = 3;
+
+console.log(x+y); // additions 
+console.log(x-y); // subtraction
+console.log(x*y); // multiplication
+console.log(x/y); //division
+console.log(x%y); //remainder
+console.log(x**3);//exponent
+
+//Assignment operator
+let a = 20;
+a += 7;
+console.log("Value of a after addition is ", a);
+a %= 3;
+console.log("Value of a after storing remainder is",a);
+a *= 5;
+console.log("value after multiplication is ",a);
+a -= 3;
+console.log("Value after subtraction is", a);
+a /= 2;
+console.log("Value after division is ", a);
+a **= 2;
+console.log("Value after exponent is ",a);
