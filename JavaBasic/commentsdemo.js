3b140cb8124f85a30e9593858f45deacc0c8cd70
Jaymeen Patel <pjaymeen908@gmail.com>
2024-03-18 18:23:01 +0530

    single line and multiple line of comments

---
+++ JavaBasic/commentsdemo.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+// this is a some random comment 
+console.log("Jaymeen"); // thsi is a console statement
+/**
+ * this is a
+ * multipule line a 
+ * code.
+ */
+
+console.log("finally");
